468685f4eba97723887b21e4e7bd939d95213bb4
weizhiqiang <598748873@qq.com>
2021-06-12 22:23:24 +0800

    删除多余注释

--- skyeye-reportdesign/src/main/java/com/skyeye/dao/ReportDataFromJsonAnalysisDao.java
+++ skyeye-reportdesign/src/main/java/com/skyeye/dao/ReportDataFromJsonAnalysisDao.java
@@ -4,10 +4,6 @@
 
 package com.skyeye.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Map;
 
--- skyeye-reportdesign/src/main/java/com/skyeye/dao/ReportDataFromXMLAnalysisDao.java
+++ skyeye-reportdesign/src/main/java/com/skyeye/dao/ReportDataFromXMLAnalysisDao.java
@@ -4,9 +4,7 @@
 
 package com.skyeye.dao;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
